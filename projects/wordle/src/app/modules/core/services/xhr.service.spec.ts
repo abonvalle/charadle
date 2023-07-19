83f58d36c1766ea9590b86312e9c3f1f4f7b3d86
abonvalle <38685992+abonvalle@users.noreply.github.com>
2023-07-19 10:45:23 +0200

    fix test

--- projects/wordle/src/app/modules/core/services/xhr.service.spec.ts
+++ projects/wordle/src/app/modules/core/services/xhr.service.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { SnackbarService } from './snackbar.service';
 import { XHRService } from './xhr.service';
 
 describe('XHRService', () => {
   let service: XHRService;
+  // let snackbarServiceSpy: jasmine.SpyObj<SnackbarService>;
   beforeEach(() => {
-    TestBed.configureTestingModule({ providers: [XHRService] });
+    const spy = jasmine.createSpyObj('SnackbarService', ['defaultErrorMsg']);
+    TestBed.configureTestingModule({ providers: [XHRService, { provide: SnackbarService, useValue: spy }] });
     service = TestBed.inject(XHRService);
+    // snackbarServiceSpy = TestBed.inject(SnackbarService) as jasmine.SpyObj<SnackbarService>;
+
     spyOn(XMLHttpRequest.prototype, 'open').and.callThrough();
     spyOn(XMLHttpRequest.prototype, 'send');
   });
